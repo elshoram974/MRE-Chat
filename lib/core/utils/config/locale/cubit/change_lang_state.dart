cd21b5d66f852d2b2298a22a1758292c3f948452
Mohammed Elshora <elshoram974@gmail.com>
2023-10-27 17:36:21 +0200

    systemLocales

---
+++ lib/core/utils/config/locale/cubit/change_lang_state.dart
@@ -0,0 +1,10 @@
+part of 'change_lang_cubit.dart';
+
+sealed class ChangeLangState extends Equatable {
+  const ChangeLangState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ChangeLangInitial extends ChangeLangState {}
