62d65569650b93598f026fcec1aba270b6bd4ea4
Mohammed Elshora <elshoram974@gmail.com>
2023-11-01 19:31:48 +0200

    connect with firebase

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -1,20 +1,34 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'auth_state.dart';
 
 enum ShowPass { login, createPass, rePass }
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(const AuthInitial());
+  AuthCubit() : super(const AuthInitial()) {
+    getUser();
+  }
   final FocusNode focusNode = FocusNode();
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
   List<bool> obscurePass = [true, true, true];
   void changeObscureLogin(ShowPass showPass) {
     emit(const AuthInitial());
     obscurePass[showPass.index] = !obscurePass[showPass.index];
     emit(const ChangeObscureState());
+  }
+
+  void getUser() {
+    auth.idTokenChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:chat/core/utils/services/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'core/utils/config/themes/app_theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
