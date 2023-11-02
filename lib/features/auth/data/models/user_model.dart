3f86147df6023cabb7348456b3c2cfba3f0921e1
Mohammed Elshora <elshoram974@gmail.com>
2023-11-02 13:51:13 +0200

    UserModel

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,42 @@
+import 'package:chat/features/auth/domain/entities/user_auth_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel extends UserAuthEntity {
+  final bool emailVerified;
+  final UserMetadata metadata;
+  final String? phoneNumber;
+  final String? photoURL;
+  final List<UserInfo> providerData;
+  final String? refreshToken;
+  final String? tenantId;
+  final String uid;
+
+  const UserModel({
+    required this.emailVerified,
+    required this.metadata,
+    this.phoneNumber,
+    this.photoURL,
+    required this.providerData,
+    this.refreshToken,
+    this.tenantId,
+    required this.uid,
+    super.name,
+    super.email,
+    super.password,
+  });
+
+  factory UserModel.fromUserCredential(User user) {
+    return UserModel(
+      emailVerified: user.emailVerified,
+      metadata: user.metadata,
+      providerData: user.providerData,
+      uid: user.uid,
+      email: user.email,
+      name: user.displayName,
+      phoneNumber: user.phoneNumber,
+      photoURL: user.photoURL,
+      refreshToken: user.refreshToken,
+      tenantId: user.tenantId,
+    );
+  }
+}
