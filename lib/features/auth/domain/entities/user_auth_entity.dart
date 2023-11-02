30efe354403b38dca03822de5cc5aad6001eb935
Mohammed Elshora <elshoram974@gmail.com>
2023-11-02 12:14:07 +0200

    user auth entity

---
+++ lib/features/auth/domain/entities/user_auth_entity.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserAuthEntity extends Equatable {
+  final String? name;
+  final String? email;
+  final String? password;
+
+  const UserAuthEntity({
+    required this.name,
+    required this.email,
+    this.password,
+  });
+
+  @override
+  List<Object?> get props => [];
+
+  factory UserAuthEntity.fromUserCredential(User user) {
+    return UserAuthEntity(
+      name: user.displayName,
+      email: user.email,
+    );
+  }
+}
